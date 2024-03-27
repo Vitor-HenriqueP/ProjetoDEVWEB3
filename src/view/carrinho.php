d0405245f2c738183f629050b8042153bd222ced
Vinicius Anderle <vinianderle00@gmail.com>
2024-03-27 16:02:44 -0300

    freete pt1

--- src/view/carrinho.php
+++ src/view/carrinho.php
@@ -57,9 +57,7 @@ $result_quantidade = $stmt_quantidade->get_result();
 $row_quantidade = $result_quantidade->fetch_assoc();
 $_SESSION['quantidade_carrinho'] = $row_quantidade['quantidade'];
 $stmt_quantidade->close();
-?>
-
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <html lang="pt-BR">
 
 <head>
@@ -83,73 +81,115 @@ $stmt_quantidade->close();
                 }
             });
         }
+
+        $(document).ready(function() {
+            $('#cep').on('input', function() {
+                var cep = $(this).val();
+                if (cep.length === 5) {
+                    $(this).val(cep + '-');
+                }
+                if (cep.length > 9) {
+                    $(this).val(cep.substring(0, 9));
+                }
+            });
+
+            $('#calcular-frete').click(function() {
+                var cep = $('#cep').val().replace('-', '');
+
+                $.getJSON('https://viacep.com.br/ws/' + cep + '/json/', function(data) {
+                    if (data.localidade) {
+                        $('#cidade').val(data.localidade);
+                    } else {
+                        $('#cidade').removeAttr('readonly').val('');
+                    }
+
+                    if (data.logradouro) {
+                        $('#rua').val(data.logradouro);
+                    } else {
+                        $('#rua').removeAttr('readonly').val('');
+                    }
+
+                    if (data.uf) {
+                        $('#estado').val(data.uf);
+                    } else {
+                        $('#estado').removeAttr('readonly').val('');
+                    }
+
+                    if (data.bairro) {
+                        $('#bairro').val(data.bairro);
+                    } else {
+                        $('#bairro').removeAttr('readonly').val('');
+                    }
+                });
+            });
+        });
     </script>
 </head>
+<h1>Carrinho</h1>
 
-<body>
-    <h1>Carrinho</h1>
+<?php
+// Consulta SQL para obter os produtos no carrinho do usuário logado
+$id_usuario = intval($_SESSION['id']);
+$stmt = $conn->prepare("SELECT c.id_produto, COUNT(c.id_produto) as quantidade, p.nome, p.descricao, p.preco, p.imagem FROM carrinho c INNER JOIN produto p ON c.id_produto = p.id WHERE c.id_usuario = ? GROUP BY c.id_produto");
+$stmt->bind_param("i", $id_usuario);
+$stmt->execute();
+$result = $stmt->get_result();
 
-    <?php
-    // Consulta SQL para obter os produtos no carrinho do usuário logado
-    $id_usuario = intval($_SESSION['id']);
-    $stmt = $conn->prepare("SELECT c.id_produto, COUNT(c.id_produto) as quantidade, p.nome, p.descricao, p.preco, p.imagem FROM carrinho c INNER JOIN produto p ON c.id_produto = p.id WHERE c.id_usuario = ? GROUP BY c.id_produto");
-    $stmt->bind_param("i", $id_usuario);
-    $stmt->execute();
-    $result = $stmt->get_result();
+if ($result->num_rows > 0) {
+    $total = 0; // Variável para armazenar o total dos produtos no carrinho
 
-    if ($result->num_rows > 0) {
-        $total = 0; // Variável para armazenar o total dos produtos no carrinho
+    while ($row = $result->fetch_assoc()) {
+        $total += $row['preco'] *
+            $row['quantidade']; // Adiciona o preço do produto ao total
+        $descricao = explode(' ', $row["descricao"]);
+        $descricao = array_slice($descricao, 0, 20);
+        $descricao = implode(' ', $descricao);
 
-        while ($row = $result->fetch_assoc()) {
-            $total += $row['preco'] *
-                $row['quantidade']; // Adiciona o preço do produto ao total
-            $descricao = explode(' ', $row["descricao"]);
-            $descricao = array_slice($descricao, 0, 20);
-            $descricao = implode(' ', $descricao);
-
-            echo "<form method='post' action='../../src/view/produto.php'>";
-            echo "<input type='hidden' name='id' value='" . $row["id_produto"] . "'>";
-            echo "<button type='submit' style='background: none; border: none; padding: 0; margin: 0;' class='product-image-button'>";
-            echo "<img src='data:image/jpeg;base64," . base64_encode($row['imagem']) . "' alt='" . $row['nome'] . "' class='product-image'>";
-            echo "</button>";
-            echo "</form>";
-
-            echo "<p>{$descricao} - R$ {$row['preco']} - Quantidade: {$row['quantidade']}</p>";
-
-            echo "<button type='button' onclick='alterarQuantidade({$row['id_produto']}, \"remove\")' class='button'>-</button>";
-
-            echo "<button type='button' onclick='alterarQuantidade({$row['id_produto']}, \"add\")' class='button'>+</button>";
-        }
-
-        // Exibe o valor total
-        echo "<p><strong>Total: R$ $total</strong></p>";
-
-        // Botão Comprar
-        echo "<form method='post'>";
-        echo "<input type='submit' name='comprar' value='Comprar' class='button'>";
+        echo "<form method='post' action='../../src/view/produto.php'>";
+        echo "<input type='hidden' name='id' value='" . $row["id_produto"] . "'>";
+        echo "<button type='submit' style='background: none; border: none; padding: 0; margin: 0;' class='product-image-button'>";
+        echo "<img src='data:image/jpeg;base64," . base64_encode($row['imagem']) . "' alt='" . $row['nome'] . "' class='product-image'>";
+        echo "</button>";
         echo "</form>";
 
-        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['comprar'])) {
-            // Limpa todos os itens do carrinho no banco de dados
-            $stmt_delete = $conn->prepare("DELETE FROM carrinho WHERE id_usuario = ?");
-            $stmt_delete->bind_param("i", $id_usuario);
-            if ($stmt_delete->execute()) {
-                // Recarrega a página após 2 segundos
-                echo "<script>setTimeout(function(){ location.reload(); }, 500);</script>";
-                echo "<div id='compra-realizada' style='background-color: #dff0d8; color: #
-                3c763d; padding: 10px; margin-top: 10px;'>Compra realizada!</div>";
-            } else {
-                echo "Erro ao realizar a compra: " . $conn->error;
-            }
-        }
-    } else {
-        echo "<p>Carrinho vazio</p>";
+        echo "<p>{$descricao} - R$ {$row['preco']} - Quantidade: {$row['quantidade']}</p>";
+
+        echo "<button type='button' onclick='alterarQuantidade({$row['id_produto']}, \"remove\")' class='button'>-</button>";
+
+        echo "<button type='button' onclick='alterarQuantidade({$row['id_produto']}, \"add\")' class='button'>+</button>";
     }
-    ?>
 
-    <a href="../../index.php">Voltar para a página inicial</a>
+    // Exibe o valor total
+    echo "<p><strong>Total: R$ $total</strong></p>";
 
-    <div class="card-mensagem" id="mensagem"><?php echo isset($mensagem) ? $mensagem : ''; ?></div>
+    // Botão Comprar
+    echo "<form method='post'>";
+    echo "<input type='submit' name='comprar' value='Comprar' class='button'>";
+    echo "</form>";
+}
+?>
+
+<?php if ($result->num_rows > 0) : ?>
+    <label for="cep">CEP:</label>
+    <input type="text" id="cep" name="cep" maxlength="9">
+    <button id="calcular-frete">Calcular Frete</button><br><br>
+
+    <!-- Campos de endereço -->
+    <label for="cidade">Cidade:</label>
+    <input type="text" id="cidade" name="cidade" readonly><br>
+    <label for="rua">Rua:</label>
+    <input type="text" id="rua" name="rua" readonly><br>
+    <label for="estado">Estado:</label>
+    <input type="text" id="estado" name="estado" readonly><br>
+    <label for="bairro">Bairro:</label>
+    <input type="text" id="bairro" name="bairro" readonly><br>
+    <label for="numero">Número:</label>
+    <input type="text" id="numero" name="numero"><br>
+<?php endif; ?>
+
+<a href="../../index.php">Voltar para a página inicial</a>
+
+<div class="card-mensagem" id="mensagem"><?php echo isset($mensagem) ? $mensagem : ''; ?></div>
 
 </body>
 
