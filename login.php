ac9f5c32bae654429af0c3a94d3af81e34d29f4a
Vinicius Anderle <vinianderle00@gmail.com>
2024-03-29 23:06:19 -0300

    organizado

--- login.php
+++ login.php
@@ -127,32 +127,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </div>
 
 
-
-    <script>
-        document.addEventListener("DOMContentLoaded", function() {
-            document.querySelector("#cadastroForm").addEventListener("submit", function(e) {
-                e.preventDefault();
-                var nome = document.querySelector("#nome").value;
-                var login = document.querySelector("#login").value;
-                var senha = document.querySelector("#senha").value;
-
-                var xhr = new XMLHttpRequest();
-                xhr.open("POST", "login.php", true);
-                xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-                xhr.onreadystatechange = function() {
-                    if (xhr.readyState == 4 && xhr.status == 200) {
-                        var response = JSON.parse(xhr.responseText);
-                        if (response.status == "success") {
-                            document.querySelector("#mensagem").innerHTML = "<p style='color:green;'>" + response.mensagem + "</p>";
-                        } else {
-                            document.querySelector("#mensagem").innerHTML = "<p style='color:red;'>" + response.mensagem + "</p>";
-                        }
-                    }
-                };
-                xhr.send("nome=" + nome + "&login=" + login + "&senha=" + senha);
-            });
-        });
-    </script>
     <script src="./src/view/assets/js/scriptlogin.js"></script>
 
 </body>
--- src/view/assets/js/scriptlogin.js
+++ src/view/assets/js/scriptlogin.js
@@ -52,4 +52,26 @@ document.addEventListener("DOMContentLoaded", function () {
         xhr.send("login=" + login + "&senha=" + senha);
     });
 });
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("#cadastroForm").addEventListener("submit", function (e) {
+        e.preventDefault();
+        var nome = document.querySelector("#nome").value;
+        var login = document.querySelector("#login").value;
+        var senha = document.querySelector("#senha").value;
 
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "login.php", true);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                var response = JSON.parse(xhr.responseText);
+                if (response.status == "success") {
+                    document.querySelector("#mensagem").innerHTML = "<p style='color:green;'>" + response.mensagem + "</p>";
+                } else {
+                    document.querySelector("#mensagem").innerHTML = "<p style='color:red;'>" + response.mensagem + "</p>";
+                }
+            }
+        };
+        xhr.send("nome=" + nome + "&login=" + login + "&senha=" + senha);
+    });
+});
